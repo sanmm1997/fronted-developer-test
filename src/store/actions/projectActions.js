04601198f8232e8b28d8e94ded84fe2cfa054714
Santiago Marulanda Molina <smarulanda97@outlook.com>
2020-02-27 07:02:02 -0500

    Remove unused function fetchStaff from store/actions/projectActions.js

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,21 +1,10 @@
 import { GET_PROJECT_INFO  } from "./../types/projectTypes";
-import {fetchProjectInfo, fetchStaff} from "../../utils/api";
+import {fetchProjectInfo} from "../../utils/api";
 
 export const getProjectInfo = () => async(dispatch) => {
   try {
     const data = await fetchProjectInfo();
     dispatch({ type: GET_PROJECT_INFO, payload: data });
-  } catch (e) {
-
-  } finally {
-
-  }
-};
-
-export const getStaffInfo = () => async (dispatch) => {
-  try {
-    const data = await fetchStaff();
-    console.log(data);
   } catch (e) {
 
   } finally {
