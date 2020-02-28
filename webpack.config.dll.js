bf9791f8adfab71f47dce342e477e6fe5a0d3202
Santiago Marulanda Molina <smarulanda97@outlook.com>
2020-02-27 20:08:04 -0500

    Update webpack.config.dll.js

--- webpack.config.dll.js
+++ webpack.config.dll.js
@@ -16,7 +16,6 @@ const entry = {
 };
 
 const output = {
-    publicPath: '/fronted-developer-test',
     library: "[name]",
     filename: 'js/[name].js',
     path: path.resolve(__dirname, 'dist'),
